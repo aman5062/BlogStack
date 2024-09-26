44b209a77f66d8bfcd70bf5305b0f37971ba1d36
Aman kumar keshri <72082692+aman5062@users.noreply.github.com>
2024-09-26 22:40:50 +0530

    ss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,35 @@
+import { Form, Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import CreatePost from './pages/CreatePost';
+import PostDetails from './pages/PostDetails';
+import EditPost from './pages/EditPost';
+import MyBlogs from './pages/MyBlogs';
+import Profile from './pages/Profile';
+import UserContextProvider from "./context/UserContext";
+
+
 
 
 function App() {
   return (
    <>
-   <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   <UserContextProvider>
 
+  
+    <Routes>
+      <Route exact path='/' element={<Home/>}  />
+      <Route exact path='/login' element={<Login/>}  />
+      <Route exact path='/register' element={<Register/>}  />
+      <Route exact path='/write' element={<CreatePost/>}  />
+      <Route exact path='/Post/post/:id' element={<PostDetails/>}  />
+      <Route exact path='/Post/post/:id' element={<EditPost/>}  />
+      <Route exact path='/myblogs/:id' element={<MyBlogs/>}  />
+      <Route exact path='/profile/:id' element={<Profile/>}  />
+
+    </Routes> 
+    </UserContextProvider>
    </>
   );
 }
--- frontend/src/Components/Menu.jsx
+++ frontend/src/Components/Menu.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../context/UserContext'
+import { UseContext } from 'react'
+import axios from 'axios'
+import { Link, useNavigation } from 'react-router-dom'
 
 function Menu() {
+const {user} = useContext(UserContext)
+const {setUser} = useContext(UserContext)
+const navigate = useNavigation()
+
+const handlelogout = async() =>{
+  try{
+    const res = await axios.get("/api/auth/logout", {withCredentials : true})
+    setUser(null)
+    navigate("/login")
+  }
+  catch(err){
+    console.log(err);
+  }
+}
   return (
-    <div>
-      
+    <div className='bg-black w-[200px] z-10 flex flex-col items-start absolute top-12 right-6 md:right-32'>
+      {
+        !user && <h3 className='text-white text-sm hover:text-gray-500 cursor-pointer'>
+          <Link to='/login'>Login</Link>
+        </h3>
+      }
+       {
+        !user && <h3 className='text-white text-sm hover:text-gray-500 cursor-pointer'>
+          <Link to='/register'>Register</Link>
+        </h3>
+      }
+       {
+        user && <h3 className='text-white text-sm hover:text-gray-500 cursor-pointer'>
+          <Link to={'/profile/'+ user_id}>Profile</Link>
+        </h3>
+      }
+      {
+        user && <h3 className='text-white text-sm hover:text-gray-500 cursor-pointer'>
+          <Link to={'/write'}>Write</Link>
+        </h3>
+      }
+      {
+        user && <h3 className='text-white text-sm hover:text-gray-500 cursor-pointer'>
+          <Link to={'/myblogs/'+ user_id}>My Blog</Link>
+        </h3>
+      }
+      {
+        user && <h3 className='text-white text-sm hover:text-gray-500 cursor-pointer' onClick={handlelogout}>
+          Logout
+        </h3>
+      }
     </div>
   )
 }
